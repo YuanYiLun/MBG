2117f88ccce0cf1625bfd4dac46165e3e3323a10
袁延昭 <yuanyanzhao@hyperchain.cn>
2019-06-27 17:24:06 +0800

    逆向工程

---
+++ src/main/java/cn/hyperchain/mbg/Main.java
@@ -0,0 +1,34 @@
+package cn.hyperchain.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * File: Main.java
+ * Description:
+ * Company:
+ * Author: yyz
+ * Datetime: 2019-06-27
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            File configFile = new File("src/main/resources/mbg.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cn/hyperchain/mbg/MybatisMbgApplication.java
@@ -0,0 +1,13 @@
+package cn.hyperchain.mbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisMbgApplication.class, args);
+	}
+
+}
